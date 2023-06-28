6306206654fed40d4539ec64244f8c20917f3fa1
mkreive <mkreive@gmail.com>
2023-06-28 11:17:25 +0300

    initial

---
+++ src/test/java/lt/javau5/onetoone/OneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.javau5.onetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
